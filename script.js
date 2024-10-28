49076471595931f6616ac84b4f8782a705a3a1f6
Su <1492889374@qq.com>
2024-10-28 09:31:04 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // 动态更新时间戳
 function updateTime(){
+
     const now = new Date();
-    const timeString = now.toLocaleDateString(); //获取当前的时间
+    const year =now.getFullYear(); //获取年
+    const month =String(now.getMonth()+1).padStart(2,'0') //获取月
+    const day =String(now.getDate()+1).padStart(2,'0') //获取天
+    const hours = String(now.getHours()).padStart(2,'0'); //格式化小时
+    const minutes = String(now.getMinutes()).padStart(2,'0'); //格式化分钟
+    const seconds = String(now.getSeconds()).padStart(2,'0'); //格式化秒
+    //构建时间字符串
+    const timeString = `${year}:${month}:${day}:${hours}:${minutes}:${seconds}`;
     document.getElementById('timestamp').textContent=timeString
 }
 // 每秒更新时间
