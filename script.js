23905b76033534c7b5a04acf3e65d0b297b85e39
Su <1492889374@qq.com>
2024-10-28 08:39:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.style.overflow = "auto";
     }, 2500);
 });
+
+// 动态更新时间戳
+function updateTime(){
+    const now = new Date();
+    const timeString = now.toLocaleDateString(); //获取当前的时间
+    document.getElementById('timestamp').textContent=timeString
+}
+// 每秒更新时间
+setInterval(updateTime,1000);
+// 初始化时间
+updateTime();
